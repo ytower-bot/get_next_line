23d8b64f944e16c7ae79e49e9701196597f5fe6a
yfaustin <yfaustin@student.42.rio>
2025-01-07 19:30:39 -0300

    add functions removed from utils.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,23 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 02:28:15 by yfaustin          #+#    #+#             */
-/*   Updated: 2024/12/21 20:30:59 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/01/07 19:28:28 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <stdlib.h>
 # include <unistd.h>
 
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strndup(const char *s, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
 char	*get_next_line(int fd);
 
 #endif
