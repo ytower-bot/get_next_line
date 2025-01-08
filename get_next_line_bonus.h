87b1f983297c7c19c735f21dba96a25186ad754b
yfaustin <yfaustin@student.42.rio>
2025-01-07 22:26:32 -0300

    bonus!

---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 20:18:33 by yfaustin          #+#    #+#             */
+/*   Updated: 2025/01/07 20:19:26 by yfaustin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strndup(const char *s, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*get_next_line(int fd);
+
+#endif
